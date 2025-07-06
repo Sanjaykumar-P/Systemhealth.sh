7ce601c1e3108a5f8a2db075e48682ec75a5f9eb
Sanjaykumar-P <sanjaysanjay55750@gmail.com>
2025-07-06 18:22:50 +0530

    Create Readme File For mail.sh

---
+++ Readme File For mail.sh
@@ -0,0 +1,26 @@
+# 📧 Email System Report Script (Using `mail` Command)
+
+This simple Bash script sends an email with a system report file as an attachment using the `mail` command.
+
+---
+
+## 📄 What the Script Does
+
+- Sends an email to `sanjaysanjay55750@gmail.com`
+- Subject: `SystemReport`
+- Message body: `Your System Report Generated`
+- Attachment: `/project9/file.txt`
+- Prints a confirmation message after sending
+
+---
+
+## 🛠️ Prerequisites
+
+Install `mailx` on your system:
+
+### For Fedora / RHEL / CentOS:
+
+```bash
+sudo dnf install mail
+
+
